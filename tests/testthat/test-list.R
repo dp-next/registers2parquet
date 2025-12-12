bd3ba285cf3c57c9aad447d78a3bd4af9dfd3a93
Signe Kirk Brødbæk <signebroedbaek@gmail.com>
2025-12-12 15:52:15 +0100

    test: :white_check_mark: add tests for `list_*()` functions

---
+++ tests/testthat/test-list.R
@@ -0,0 +1,58 @@
+# Prepare temp files
+temp_dir <- fs::path_temp("temp_dir")
+fs::dir_create(temp_dir)
+
+sas_file1 <- fs::path(temp_dir, "file1.sas7bdat")
+sas_file2 <- fs::path(temp_dir, "subdir", "file2.sas7bdat")
+sas_file3 <- fs::path(temp_dir, "file3.SAS7BDAT")
+parquet_file1 <- fs::path(temp_dir, "file1.parquet")
+parquet_file2 <- fs::path(temp_dir, "subdir", "file2.parq")
+parquet_file3 <- fs::path(temp_dir, "file3.PARQUET")
+
+fs::file_create(sas_file1)
+fs::dir_create(fs::path_dir(sas_file2))
+fs::file_create(sas_file2)
+fs::file_create(sas_file3)
+fs::file_create(parquet_file1)
+fs::file_create(parquet_file2)
+fs::file_create(parquet_file3)
+
+test_that("expected SAS files are listed", {
+  expected <- sort(c(sas_file1, sas_file2, sas_file3))
+  actual <- list_sas_registers(temp_dir)
+
+  expect_equal(as.character(actual), as.character(expected))
+})
+
+test_that("expected Parquet files are listed", {
+  expected <- sort(c(parquet_file1, parquet_file2, parquet_file3))
+  actual <- list_parquet_registers(temp_dir)
+
+  expect_equal(as.character(actual), as.character(expected))
+})
+
+test_that("an empty list is returned when no relevant files are found", {
+  no_relevant_files_dir <- fs::path_temp("no_relevant_files")
+  fs::dir_create(no_relevant_files_dir)
+  fs::file_create(fs::path(no_relevant_files_dir, "unrelated.txt"))
+
+  sas_files <- list_sas_registers(no_relevant_files_dir)
+  parquet_files <- list_parquet_registers(no_relevant_files_dir)
+
+  expect_equal(length(sas_files), 0)
+  expect_equal(length(parquet_files), 0)
+})
+
+test_that("error is thrown for non-existent path", {
+  non_existent_dir <- fs::path_temp("non_existent")
+
+  expect_error(
+    list_sas_registers(non_existent_dir),
+    regexp = "does not exist"
+  )
+
+  expect_error(
+    list_parquet_registers(non_existent_dir),
+    regexp = "does not exist"
+  )
+})
