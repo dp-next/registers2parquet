b888fd2adaeb25db4f7080d8e0d67ac39918dcd6
Signe Kirk Brødbæk <signebroedbaek@gmail.com>
2026-01-27 14:40:36 +0100

    test: :white_check_mark: add `n` param to `helper_create_simulated_kontakter()`

--- tests/testthat/helper-create-simulated-data.R
+++ tests/testthat/helper-create-simulated-data.R
@@ -1,7 +1,15 @@
-helper_create_simulated_kontakter <- function() {
+#' Create simulated kontakter registers to be used in tests
+#'
+#' @param n Number of rows to simulate per simulated kontakter tibble.
+#'
+#' @returns A named list with three tibbles containing simulated kontakter
+#'  registers.
+#'
+#' @keywords internal
+helper_create_simulated_kontakter <- function(n = 1000000) {
   list(
-    kontakter = osdc::simulate_registers("kontakter", n = 1000000)[[1]],
-    kontakter_1999_1 = osdc::simulate_registers("kontakter", n = 1000000)[[1]],
-    kontakter_1999_2 = osdc::simulate_registers("kontakter", n = 1000000)[[1]]
+    kontakter = osdc::simulate_registers("kontakter", n = n)[[1]],
+    kontakter_1999_1 = osdc::simulate_registers("kontakter", n = n)[[1]],
+    kontakter_1999_2 = osdc::simulate_registers("kontakter", n = n)[[1]]
   )
 }
