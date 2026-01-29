ee9c36997edd0bf5a2910193116f44c2b1a4dbbb
Signe Kirk Brødbæk <signebroedbaek@gmail.com>
2026-01-29 17:02:08 +0100

    docs: :memo: update roxygen `@description` section

--- R/convert.R
+++ R/convert.R
@@ -5,12 +5,14 @@
 #' data in Parquet format. It expects the input SAS files to come from the same
 #' register, e.g., different years of the same register.
 #'
-#' If multiple paths are given, the function looks for a year (the first four
-#' consecutive digits) in the file names to use the year as partition, see
-#' `vignettes("design")` for more information about the partitioning. If a year
-#' is found, the data is saved partitioned by year in the output directory,
-#' e.g., `path/to/register_name/year=2020/part-ad5b.parquet` (the ending being an UUID). If no year is
-#' found in the file name, the data is still partitioned with `year=NA`.
+#' The function looks for a year (the first four consecutive digits) in the file
+#' names in `file_paths` to use the year as partition, see `vignettes("design")`
+#' for more information about the partitioning.
+#'
+#' If a year is found, the data is saved partitioned by year in the output
+#' directory, e.g., `path/to/register_name/year=2020/part-ad5b.parquet` (the
+#' ending being an UUID). If no year is found in the file name, the data is
+#' still partitioned with `year=NA`.
 #'
 #' Because this function only converts one file at a time (in chunks) to be
 #' able to handle larger-than-memory SAS files, duplicate rows across files are
