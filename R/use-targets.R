d06833066e6e0a822e30352af8ca73664dbe9062
Signe Kirk Brødbæk <40836345+signekb@users.noreply.github.com>
2026-02-05 15:43:36 +0100

    refactor: 🚚 `use_targets_pipeline()` -> `use_targets()` (#145)
    # Description
    
    Needs no review.
    
    ## Checklist
    
    - [X] Ran `just run-all`

--- R/use-targets.R
+++ R/use-targets.R
@@ -1,6 +1,6 @@
-#' Set up a targets pipeline for converting SAS registers to Parquet
+#' Use a targets pipeline template for converting SAS registers to Parquet
 #'
-#' Copies the targets pipeline template to your project root.
+#' Copies a template to your project root.
 #'
 #' @param path Path to the file to create. Defaults to `_targets.R`.
 #' @param open Whether to open the file for editing. Defaults to `TRUE` in
@@ -9,10 +9,12 @@
 #' @returns The path to the created file (invisibly).
 #'
 #' @export
-use_targets_pipeline <- function(
+use_targets <- function(
   path = "_targets.R",
   open = rlang::is_interactive()
 ) {
+  checkmate::assert_string(fs::path_file(path), "_targets.R")
+
   template_path <- fs::path_package("fastreg", "template-targets.R")
 
   if (fs::file_exists(path)) {
@@ -22,7 +24,7 @@ use_targets_pipeline <- function(
     ))
   }
 
-  fs::file_copy(template_path, path)
+  fs::file_copy(path = template_path, new_path = path)
 
   if (fs::file_exists(path)) {
     cli::cli_alert_success("Created {.file {path}}")
