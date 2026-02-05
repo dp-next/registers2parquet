13431ba244646f7ece8eb19d86146e0c3d6f9844
Signe Kirk Brødbæk <signebroedbaek@gmail.com>
2026-02-05 15:01:27 +0100

    refactor: :recycle: add check that `path` file name is `_targets.R`

--- R/use-targets.R
+++ R/use-targets.R
@@ -1,6 +1,6 @@
-#' Set up a targets pipeline for converting SAS registers to Parquet
+#' Use a targets pipeline template for converting SAS registers to Parquet
 #'
-#' Copies the targets pipeline template to your project root.
+#' Copies a template to your project root.
 #'
 #' @param path Path to the file to create. Defaults to `_targets.R`.
 #' @param open Whether to open the file for editing. Defaults to `TRUE` in
@@ -13,6 +13,8 @@ use_targets <- function(
   path = "_targets.R",
   open = rlang::is_interactive()
 ) {
+  checkmate::assert_string(fs::path_file(path), "_targets.R")
+
   template_path <- fs::path_package("fastreg", "template-targets.R")
 
   if (fs::file_exists(path)) {
