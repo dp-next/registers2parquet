a34f05718366872af47667825b926219a2e91389
Signe Kirk Brødbæk <40836345+signekb@users.noreply.github.com>
2026-02-06 15:56:30 +0100

    feat: :sparkles: simulate register functions; test clean-up (#146)
    # Description
    
    I want to use some simulated data in the guide, so I've moved the helper
    to create simulated `kontakter` data to `R/` and added a `diagnoser` one
    as well. In the tests, there was quite a lot of setup (messy), so I
    added a `save_as_sas()` helper too. This lead to quite the refactor of
    the tests to use these functions instead - as well as some general clean
    up. I found it difficult to separate the clean-up related to these new
    functions from general clean-up, but I hope this isn't too messy.
    Otherwise, let me know.
    
    Needs a thorough review.
    
    ## Checklist
    
    - [X] Ran `just run-all`

---
+++ R/simulate.R
@@ -0,0 +1,57 @@
+#' Create example kontakter registers
+#'
+#' The data is simulated using `osdc::simulate_registers()`. It's used
+#' in vignettes and tests.
+#'
+#' @param n Number of rows to simulate per simulated kontakter tibble.
+#'
+#' @returns A named list with four tibbles containing simulated kontakter
+#'  registers.
+simulate_kontakter_register <- function(
+  n = 1000
+) {
+  data_list <- list(
+    kontakter = osdc::simulate_registers("kontakter", n = n)[[1]],
+    kontakter_1999_1 = osdc::simulate_registers("kontakter", n = n)[[1]],
+    kontakter_1999_2 = osdc::simulate_registers("kontakter", n = n)[[1]],
+    kontakter_2020 = osdc::simulate_registers("kontakter", n = n)[[1]]
+  )
+}
+
+#' Create example diagnoser registers
+#'
+#' The data is simulated using `osdc::simulate_registers()`. It's used
+#' in vignettes and tests.
+#'
+#' @param n Number of rows to simulate per simulated diagnoser tibble.
+#'
+#' @returns A named list with two tibbles containing simulated diagnoser
+#'  registers.
+simulate_diagnoser_register <- function(
+  n = 1000
+) {
+  data_list <- list(
+    diagnoser_2020 = osdc::simulate_registers("diagnoser", n = n)[[1]],
+    diagnoser_2021 = osdc::simulate_registers("diagnoser", n = n)[[1]]
+  )
+}
+
+#' Save a list of data frames as SAS files
+#'
+#' Writes each element of a named list as a SAS file to the given directory.
+#' The file names are derived from the list names.
+#'
+#' @param data_list A named list of data frames.
+#' @param path A character scalar with the directory to save the SAS files to.
+#'
+#' @returns The path invisibly.
+save_as_sas <- function(data_list, path) {
+  fs::dir_create(path, recurse = TRUE)
+  purrr::iwalk(data_list, \(df, name) {
+    suppressWarnings(haven::write_sas(
+      df,
+      fs::path(path, paste0(name, ".sas7bdat"))
+    ))
+  })
+  invisible(path)
+}
