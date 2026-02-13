83d218d3075d5869b69cd30af7b7122e736ee783
Signe Kirk Brødbæk <signebroedbaek@gmail.com>
2026-02-13 16:19:26 +0100

    fix: :pencil2: `temp_path` -> `path_temp`

--- R/use-targets.R
+++ R/use-targets.R
@@ -9,7 +9,7 @@
 #'
 #' @export
 #' @examples
-#' use_targets_template(path = fs::temp_path("_targets.R"))
+#' use_targets_template(path = fs::path_temp("_targets.R"))
 use_targets_template <- function(
   path = "_targets.R",
   open = rlang::is_interactive()
