933737c68adc7df5922d8ecfe4e8e54badeb0a05
Luke W. Johnston <lwjohnst@gmail.com>
2026-02-17 21:02:34 +0100

    refactor: :fire: not using `list_parquet_files()` anymore

--- R/list.R
+++ R/list.R
@@ -30,37 +30,3 @@ list_sas_files <- function(path) {
 
   sas_files
 }
-
-#' List Parquet files in a directory
-#'
-#' Lists all Parquet register files (with the extension `.parquet`
-#' or `.parq` case-insensitively) in the specified directory and its
-#' subdirectories.
-#'
-#' @inheritParams list_sas_files
-#'
-#' @returns The path(s) to the found Parquet file(s).
-#'
-#' @export
-#' @examples
-#' list_parquet_files(fs::path_package("fastreg", "extdata"))
-list_parquet_files <- function(path) {
-  # Check input.
-  checkmate::assert_directory(path)
-  checkmate::assert_string(path)
-
-  # List all Parquet files in the directory and its subdirectories.
-  # (?i) makes the regex case-insensitive.
-  parquet_files <- fs::dir_ls(
-    path,
-    regexp = "(?i)\\.(parquet|parq)$",
-    recurse = TRUE
-  ) |>
-    sort()
-
-  if (length(parquet_files) == 0) {
-    cli::cli_abort("No Parquet files found in {.path {path}}.")
-  }
-
-  parquet_files
-}
