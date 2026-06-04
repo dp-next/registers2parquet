a789b019a224c7c92b6b513b752047b6af3c5820
Signe Kirk Brødbæk <40836345+signekb@users.noreply.github.com>
2026-06-04 10:00:53 +0200

    feat: ✨ conversion log schema comparison (#301)
    # Description
    
    This adds an exported `print_log_schema()` function with quite a few
    helpers.
    
    Closes #287 
    Closes #4 
    
    Needs a thorough review.
    
    ## Checklist
    
    - [X] Ran `just run-all`
    
    ---------
    
    Co-authored-by: Luke W. Johnston <lwjohnst86@users.noreply.github.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- R/log.R
+++ R/log.R
@@ -24,3 +24,140 @@ print_log_row_count <- function(log) {
 
   invisible(log)
 }
+
+
+#' Print log schema comparison
+#'
+#' Prints the log schema information in a section that compares the schemas
+#' within one register. Finds the most common schema and if there's differences
+#' between schemas, it prints these differences.
+#'
+#' @param register_log A tibble returned by [convert()], filtered to only
+#'   contain rows from a single register.
+#'
+#' @returns register_log invisibly.
+#'
+#' @export
+#' @examples
+#' sas_file <- fs::path_package("fastreg", "extdata", "test.sas7bdat")
+#' log <- convert(sas_file, output_dir = fs::path_temp("output"))
+#' print_log_schema(log)
+print_log_schema <- function(register_log) {
+  file_schemas <- register_log |>
+    # Only keep first chunk per file.
+    dplyr::slice_head(n = 1, by = "input_path") |>
+    dplyr::mutate(
+      input_file = fs::path_file(.data$input_path) |> fs::path_ext_remove()
+    ) |>
+    dplyr::select(c("input_file", "schema"))
+
+  # If two schemas occur with the same frequency, only one is chosen as ref.
+  reference <- dplyr::count(file_schemas, .data$schema) |>
+    dplyr::slice_max(.data$n, with_ties = FALSE)
+  # Get schema tibble, instead of list.
+  reference_schema <- reference$schema[[1]]
+  n_reference <- reference$n
+  n_total <- nrow(file_schemas)
+  has_diffs <- n_reference < n_total
+
+  # Prepare schema difference lines.
+  description <- "All files in this register share the same schema."
+  schema_differences <- ""
+  if (has_diffs) {
+    description <- glue::glue(
+      "The most common schema occurs in {n_reference}/{n_total} files."
+    )
+
+    schema_differences <- c(
+      "### Schema differences",
+      "",
+      "Files with schemas differing from the most common (only showing differing columns):",
+      "",
+      get_schema_diffs(file_schemas, reference_schema) |>
+        chunk_diff_table() |>
+        purrr::map_chr(collapse_kable)
+    )
+  }
+
+  lines <- c(
+    # Description.
+    description,
+    # Reference schema.
+    collapse_kable(reference_schema),
+    # Schema differences.
+    schema_differences
+  )
+  cat(glue::glue_collapse(lines, sep = "\n\n"), "\n")
+
+  invisible(register_log)
+}
+
+#' @noRd
+collapse_kable <- function(table) {
+  glue::glue_collapse(knitr::kable(table), "\n")
+}
+
+
+#' Get schema differences
+#'
+#' @param schema_log The register log with the schemas.
+#' @param reference_schema The most common schema.
+#'
+#' @returns A wide-format tibble with the files and columns that differ from the
+#'   reference schema.
+#'
+#' @keywords internal
+#' @noRd
+get_schema_diffs <- function(schema_log, reference_schema) {
+  diffs <- dplyr::anti_join(
+    schema_log,
+    tibble::tibble(schema = list(reference_schema))
+  )
+
+  diffs |>
+    tidyr::unnest("schema") |>
+    # Complete with column names from the reference schema in case the diffs
+    # are missing any columns.
+    tidyr::complete(
+      .data$input_file,
+      column_name = unique(c(column_name = reference_schema$column_name))
+    ) |>
+    # Pivot so file names are columns.
+    tidyr::pivot_wider(names_from = "input_file", values_from = "data_type") |>
+    # Add reference data_type column.
+    dplyr::left_join(reference_schema, by = "column_name") |>
+    # Keep only schema columns where at least one file has a different type or
+    # is missing the column.
+    dplyr::filter(
+      dplyr::if_any(
+        c(-"column_name", -"data_type"),
+        \(col_type) {
+          is.na(.data$data_type) | is.na(col_type) | col_type != .data$data_type
+        }
+      )
+    ) |>
+    dplyr::select(-"data_type")
+}
+
+
+#' Chunk table with schema differences
+#'
+#' @param diff_table The tibble output by `get_schema_diffs()`.
+#' @param max_file_cols The maximum number of files to include in the table
+#'   column. If there's more files than this, the tibble will be chunked into
+#'   multiple tables for printing purposes.
+#'
+#' @returns A list of tibble(s).
+#'
+#' @keywords internal
+#' @noRd
+chunk_diff_table <- function(diff_table, max_file_cols = 6L) {
+  file_cols <- names(diff_table) |> setdiff("column_name")
+  chunked_file_cols <- split(
+    file_cols,
+    ceiling(seq_along(file_cols) / max_file_cols)
+  )
+  purrr::map(chunked_file_cols, \(cols) {
+    dplyr::select(diff_table, "column_name", dplyr::all_of(cols))
+  })
+}
--- tests/testthat/test-log.R
+++ tests/testthat/test-log.R
@@ -1,28 +1,72 @@
 # Setup ------------------------------------------------------------------------
 
-sas_file <- fs::path_package("fastreg", "extdata", "test.sas7bdat")
-log <- convert(
-  sas_file,
-  output_dir = fs::path_temp("output")
-)
+test_output_dir <- fs::path_temp()
+fs::dir_create(test_output_dir)
 
-# rbind with itself to get multiple logs.
-log <- log |> rbind(log)
+# Create SAS files.
+bef <- simulate_registers_with_paths("bef", c("1999", "2020"))
+lmdb_no_atc <- simulate_registers_with_paths(
+  "lmdb",
+  as.character(c(2030:2033))
+) |>
+  dplyr::mutate(
+    data = purrr::map(data, \(d) {
+      d |>
+        dplyr::select(-atc)
+    })
+  )
+
+lmdb_edited_cols <- simulate_registers_with_paths(
+  "lmdb",
+  as.character(c(2040:2043))
+) |>
+  dplyr::mutate(
+    data = purrr::map(data, \(d) {
+      d |>
+        dplyr::mutate(pnr = as.numeric(pnr), new_col = 3)
+    })
+  )
+
+lmdb <- simulate_registers_with_paths(
+  "lmdb",
+  as.character(c(2020:2025, 2045))
+) |>
+  rbind(lmdb_no_atc, lmdb_edited_cols)
+
+
+bef |> purrr::pwalk(write_to_sas)
+lmdb |> purrr::pwalk(write_to_sas)
+
+log_bef <- bef$output_path |>
+  purrr::map(\(path) {
+    convert(path, output_dir = test_output_dir)
+  }) |>
+  purrr::list_rbind()
+
+
+log_lmdb <- lmdb$output_path |>
+  purrr::map(\(path) {
+    convert(path, output_dir = test_output_dir)
+  }) |>
+  purrr::list_rbind()
 
 # Output.
-log_row_count <- capture.output(print_log_row_count(log))
+log_row_count <- capture.output(print_log_row_count(log_bef))
+log_schema_no_diff <- capture.output(print_log_schema(log_bef))
+log_schema_diff <- capture.output(print_log_schema(log_lmdb))
+
 
 # Test print_log_row_count() --------------------------------------------------------
 
 test_that("print_log_row_count() returns input invisibly", {
-  actual <- expect_invisible(print_log_row_count(log))
-  expect_equal(actual, log)
+  actual <- expect_invisible(print_log_row_count(log_bef))
+  expect_equal(actual, log_bef)
 })
 
-test_that("print_log_row_count() renders path values in output", {
+test_that("print_log_row_count() renders SAS paths in output", {
   expect_match(
     log_row_count,
-    "extdata/test.sas7bdat",
+    ".sas7bdat",
     all = FALSE,
     fixed = TRUE
   )
@@ -40,4 +84,40 @@ test_that("print_log_row_count() renders row count values in output", {
 test_that("print_log_row_count() excludes register_name and columns column", {
   expect_no_match(log_row_count, "register_name")
   expect_no_match(log_row_count, "columns")
+})
+
+# Test print_log_schema() ------------------------------------------------------
+
+test_that("print_log_schema() returns input invisibly", {
+  actual <- expect_invisible(print_log_schema(log_bef))
+  expect_equal(actual, log_bef)
+})
+
+test_that("print_log_schema() only include expected elements with no diff", {
+  # Description phrasing.
+  expect_true(any(stringr::str_detect(log_schema_no_diff, "same schema")))
+  # No mentions of differences throughout.
+  expect_false(any(stringr::str_detect(log_schema_no_diff, "differences")))
+  # Only one table (one reference).
+  n_tables <- log_schema_no_diff |>
+    stringr::str_detect("^\\|[:-]") |>
+    sum()
+  expect_equal(n_tables, 1)
+})
+
+test_that("print_log_schema() includes expected elements with diffs", {
+  # Description phrasing.
+  expect_true(any(stringr::str_detect(log_schema_diff, "most common")))
+  # Fraction of schemas matching most common schema/total files.
+  expect_match(log_schema_diff, "7/15", all = FALSE, fixed = TRUE)
+  # Differences header.
+  expect_true(any(stringr::str_detect(
+    log_schema_diff,
+    "Schema differences"
+  )))
+  # Three tables (one reference, two with schema diffs).
+  n_tables <- log_schema_diff |>
+    stringr::str_detect("^\\|[:-]") |>
+    sum()
+  expect_equal(n_tables, 3)
 })
