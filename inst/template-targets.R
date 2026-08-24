c717fb75fdc55c8c315dba8cca9ae1ba2ddf6f72
Signe Kirk Brødbæk <signebroedbaek@gmail.com>
2026-08-24 16:24:23 +0200

    feat: :sparkles: stop pipeline if `output_dir` exists and is non-empty

--- inst/template-targets.R
+++ inst/template-targets.R
@@ -59,18 +59,27 @@ list(
     command = config$sas_paths
   ),
 
-  # Empty output directory before writing to avoid outdated Parquet files.
+  # Stop the pipeline if the output directory isn't empty to avoid duplicating
+  # data from a previous conversion. Create it, if it doesn't exist.
   tar_target(
     name = output_dir,
     command = {
-      if (fs::dir_exists(config$output_dir)) {
-        fs::dir_delete(config$output_dir)
+      if (
+        fs::dir_exists(config$output_dir) &&
+          !fs::is_dir_empty(config$output_dir)
+      ) {
+        stop(
+          "Output directory is not empty: ",
+          config$output_dir,
+          ". Remove it or choose a different `output_dir` before running the pipeline.",
+          call. = FALSE
+        )
       }
       fs::dir_create(config$output_dir)
       config$output_dir
     },
     deployment = "main",
-    # Always run to ensure a clean slate.
+    # Always check the output directory before proceeding.
     cue = tar_cue(mode = "always")
   ),
 
